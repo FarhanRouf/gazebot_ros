75a6f9ce1219cae5923091a6d794a628fd4a7cf6
Faravi <60047006+ResetDissonance@users.noreply.github.com>
2025-07-15 12:29:42 -0400

    Working robot, two wheels with a load

--- README.md
+++ README.md
@@ -5,3 +5,19 @@ This is a GitHub template. You can make your own copy by clicking the green "Use
 It is recommended that you keep the repo/package name the same, but if you do change it, ensure you do a "Find all" using your IDE (or the built-in GitHub IDE by hitting the `.` key) and rename all instances of `my_bot` to whatever your project's name is.
 
 Note that each directory currently has at least one file in it to ensure that git tracks the files (and, consequently, that a fresh clone has direcctories present for CMake to find). These example files can be removed if required (and the directories can be removed if `CMakeLists.txt` is adjusted accordingly).
+
+
+
+STEP 1:
+colcon build --symlink-install
+STEP 2:
+source install/setup.bash
+STEP 3:
+ros2 launch gazebot_ros launch_sim.launch.py
+Gazebo opens and you can use the teleop on Gazebo or do the next step to control it from terminal
+STEP 4:
+ros2 run teleop_twist_keyboard teleop_twist_keyboard cmd_vel:=/diff_cont/cmd_vel_unstamped
+
+EXTRA:
+You can also run rviz2 to see how ROS2 is seeing these topics and joint states.
+ROS2 works with Gazebo through the ros_gz bridge, read gz_bridge.yaml under the src/config file.
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,8 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -22,28 +23,51 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control':'false'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'empty.world'
+    )
+    
+    world = LaunchConfiguration('world')
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value = default_world,
+        description='World to load'
+    )
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                launch_arguments={'gz_args': 'empty.sdf'}.items()
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown':'true'}.items()
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = IncludeLaunchDescription(
-                      PythonLaunchDescriptionSource([os.path.join(
-                          get_package_share_directory('ros_gz_sim'),'launch','gz_spawn_model.launch.py')]),
-                      launch_arguments={'world': 'empty','topic':'robot_description','entity_name':'bot_name','x':'0.0','y':'0.0','z':'0.2'}.items()
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'my_bot',
+                                   '-z','0.25'],
+                        output='screen')
+    
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
     )
-
-
 
     # Launch them all!
     return LaunchDescription([
         rsp,
+        world_arg,
         gazebo,
         spawn_entity,
+        ros_gz_bridge
     ])
